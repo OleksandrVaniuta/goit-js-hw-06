27efb153f4930bb13dfa52d338de2a21dabbe589
OleksandrVaniuta <vaniutasa@gmail.com>
2023-01-08 23:04:50 +0200

    зробили 3 задачі

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,11 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+const ingredientsList = document.querySelector("#ingredients");
+const ingredientEl = ingredients.map(ingredient => {
+	const item = document.createElement("li");
+	item.textContent = ingredient;
+	item.classList.add("item");
+
+	return item;
+});
+console.log(ingredientEl);
+ingredientsList.append(...ingredientEl);
